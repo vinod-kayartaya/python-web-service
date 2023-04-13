8e57ce63dbf585b5e20b4f83afb21936548f28a3
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-04-13 12:51:29 +0530

    added hello() and route /hello

--- main.py
+++ main.py
@@ -9,5 +9,10 @@ def index():
     return '<h1>Hello, world</h1>'
 
 
+@app.route('/hello')
+def hello():
+    return '<h1>Hello</h1>';
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
