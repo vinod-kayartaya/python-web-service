f7812b1872a8c7610de5d263ff83d6d283d1bdd7
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-04-13 12:34:42 +0530

    added flask code to main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # this is the entirypoint for the project
+
+from flask import Flask
+
+app = Flask()
+
+@app.route('/')
 def index():
-    pass
+    return '<h1>Hello, world</h1>'
 
 
 if __name__ == '__main__':
-    pass
+    app.run(host='0.0.0.0', port=8080)
